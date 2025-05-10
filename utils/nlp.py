167c29d3175929d7196e1b5b8ae53ef5742a0134
viv <69812396+vivienneprince@users.noreply.github.com>
2025-05-10 00:34:29 -0600

    chore: remove unused code

--- utils/nlp.py
+++ utils/nlp.py
@@ -29,18 +29,3 @@ def extract_json_from_response(json_response: str) -> dict:
         log.error(f"An unexpected error occurred: {e}")
         data = {}
     return data
-
-
-def string_to_stream(string: str):
-    """
-    Converts a string into an OpenAI stream format.
-    
-    Args:
-        string (str): The string to split.
-        
-    Returns:
-        list[str]: A list of strings, each containing one line.
-    """
-    # ChatCompletionChunk(id='ntSh7Z5-zqrih-93d72597d9d2e673', choices=[Choice(delta=ChoiceDelta(content='', function_call=None, role='assistant', tool_calls=None, token_id=None), finish_reason='stop', index=0, logprobs=None, text='')], created=1746856098, model='meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8', object='chat.completion.chunk', service_tier=None, system_fingerprint=None, usage=None),
-    # ChatCompletionChunk(id='ntSh7Z5-zqrih-93d72597d9d2e673', choices=[], created=1746856098, model='meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8', object='chat.completion.chunk', service_tier=None, system_fingerprint=None, usage=CompletionUsage(completion_tokens=33, prompt_tokens=132, total_tokens=165))]
-    return string.splitlines()
